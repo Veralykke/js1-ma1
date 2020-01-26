9d80de728381c9eda3316d08930161ca4ab8756a
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-01-26 13:41:14 +0100

    Assignment done

---
+++ vera-kaasen-js1-ma1.js
@@ -0,0 +1,122 @@
+//Question: 1
+
+//let cat = {
+//name: "Felix",
+//age: 2,
+//complain: function() {
+//console.log("Meoow");
+//}
+//}
+
+//cat.complain();
+
+//Question:2 ??
+
+//const heading = document.querySelector("h3");
+//console.dir(heading);
+
+//const heading = document.querySelector(".Subheading");
+//console.dir(heading);
+
+//console.log(heading);
+
+//Question: 3 ??
+
+//heading.style.fontSize = "2em";
+
+
+//Question: 4 ??
+
+//heading.classList.add("Subheading");
+
+
+//document.querySelector(".h3").add("subheading")
+
+//Question: 5 ???
+
+//const paragraphs = document.querySelectorAll("p");
+//console.dir(paragraphs);
+
+//Question: 6 ??
+
+//const resultContainer = document.querySelector(".results")
+//resultContainer.innerHTML = "New paragraps";
+
+//Question: 7
+
+//function cat(catArray) {
+//console.log(catArray);
+//for (let i = 0; i < catArray.length; i++) {
+//console.log(catArray[i]);
+//}
+//}
+
+//const cats = [{
+//name: "Blob",
+//age: 10
+//},
+//{
+//name: "Harold",
+//},
+//{
+//name: "Blurt",
+//age: 21
+//}
+//];
+
+//cat(cats);
+
+//Question: 8
+
+//const cats = [{
+//name: "Blob",
+//age: 10
+//},
+//{
+//name: "Harold",
+//},
+//{
+//name: "Blurt",
+//age: 21
+//}
+//];
+
+
+//function cat(catArray) {
+/* lagrer en  foreløbig tom variabel*/
+//let catNames = "";
+
+
+//for (let i = 0; i < catArray.length; i++) {
+
+//catNames += "<h5>" + catArray[i].name + "</h5>\n";
+
+//}
+
+//return catNames;
+//}
+//console.log(cat(cats));
+
+
+//Question: 9
+
+
+//resultContainer.innerHTML = cat(cats);
+
+
+//Question: 10
+
+
+//function cat(catArray) {
+/* lagrer en  foreløbig tom variabel*/
+//let catNames = "";
+
+//for (let i = 0; i < catArray.length; i++) {
+//if (catArray[i].age === undefined) {
+//catArray[i].age = "Age unknown";
+//}
+//catNames += "<div>" + "<h5>" + catArray[i].name + "</h5>\n" + "<p>" + catArray[i].age + "<p>" + "</div>"
+//}
+
+//return catNames;
+//}
